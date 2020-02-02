dc5bee4a6577bf93818a32b642809636d4d6997a
Drew Dennison <drew@returntocorp.com>
2020-02-01 22:33:51 -0800

    update file

--- tests/self_test.py
+++ tests/self_test.py
@@ -5,3 +5,15 @@ if a + b == b + a:
     print('inside')
 else:
     print('outside')
+
+if 1 == 1:
+   print('true')
+
+if a == b:
+   print('asdf')
+
+if a == a:
+   print("foobar")
+
+if 1+2 == 2+1:
+   print("bar")
