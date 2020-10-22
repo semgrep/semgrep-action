d1f220ebcd6ab45e66e5cc913be323658d60fe07
Sabrina Brogren <sabrina@returntocorp.com>
2020-10-22 18:51:30 -0400

    remove commenting code, send token

--- src/semgrep_agent/constants.py
+++ src/semgrep_agent/constants.py
@@ -1,4 +1,3 @@
 SUPPORT_EMAIL = "support@r2c.dev"
 
 PRIVACY_SENSITIVE_FIELDS = {"syntactic_context"}
-RULE_BUG_REPORT = "https://github.com/returntocorp/semgrep-rules/issues/new?assignees=&labels=&template=bug_report.md&title="
