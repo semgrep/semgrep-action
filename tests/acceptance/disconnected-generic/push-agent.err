semgrep/semgrep-action

commit 550275f02a71b675773d108f2f08c98658ce87cf

Allow passing any arg with --semgrep-opts (#147)
Tested by running:
python -m semgrep_agent --config p/r2c --baseline-ref HEAD~3 \
--semgrep-opts '--max-lines-per-finding 45 -j "2" --timeout 90'

src/semgrep_agent/main.py
@@ -57,10 +57,11 @@ def get_aligned_command(title: str, subtext: str) -> str:
 )
 @click.option("--publish-token", envvar="INPUT_PUBLISHTOKEN", type=str)
 @click.option("--publish-deployment", envvar="INPUT_PUBLISHDEPLOYMENT", type=int)
-@click.option("--json", "json_output", hidden=True, is_flag=True)
 @click.option(
     "--gitlab-json", "gitlab_output", envvar="SEMGREP_GITLAB_JSON", is_flag=True
 )
+@click.option("--json", "json_output", hidden=True, is_flag=True)
+@click.option("--semgrep-opts", hidden=True, type=str)
 def main(
     config: str,
     baseline_ref: str,
@@ -69,6 +70,7 @@ def main(
     publish_deployment: int,
     json_output: bool,
     gitlab_output: bool,
+    semgrep_opts: str,
 ) -> NoReturn:
     click.echo("=== detecting environment", err=True)
     click.echo(
@@ -175,6 +177,7 @@ def main(
         meta.head_ref,
         semgrep.get_semgrepignore(sapp.scan.ignore_patterns),
         sapp.is_configured,
+        semgrep_opts=semgrep_opts,
     )
     new_findings = results.findings.new
 

src/semgrep_agent/semgrep.py
@@ -1,12 +1,14 @@
 import io
 import json
 import os
+import shlex
 import sys
 import time
 import urllib.parse
 from contextlib import contextmanager
 from dataclasses import dataclass
 from datetime import datetime
+from json.decoder import JSONDecodeError
 from pathlib import Path
 from textwrap import dedent
 from textwrap import indent
@@ -44,6 +46,16 @@ semgrep = sh.semgrep.bake(_ok_code={0, 1}, _tty_out=False, _env=ua_environ)
 # a typical old system has 128 * 1024 as their max command length
 # we assume an average ~250 characters for a path in the worst case
 PATHS_CHUNK_SIZE = 500
+
+
+class SemgrepCommandFailure(Exception):
+    """This imitates ``sh.ErrorReturnCode``, but is raised when Semgrep fails with exit code 0."""
+
+    def __init__(self, command: sh.RunningCommand):
+        self.stdout = command.stdout
+        self.stderr = command.stderr
+        self.full_cmd = b" ".join(command.cmd).decode()
+        self.exit_code = command.exit_code
 
 
 def resolve_config_shorthand(config: str) -> str:
@@ -166,6 +178,8 @@ def invoke_semgrep(
     head_ref: Optional[str],
     semgrep_ignore: TextIO,
     uses_managed_policy: bool,
+    *,
+    semgrep_opts: str,
 ) -> FindingSets:
     debug_echo("=== adding semgrep configuration")
 
@@ -180,6 +194,7 @@ def invoke_semgrep(
 
         config_args = ["--config", config_specifier]
         rewrite_args = ["--no-rewrite-rule-ids"] if uses_managed_policy else []
+        user_provided_args = shlex.split(semgrep_opts)  # incompatible with Windows
 
         debug_echo("=== seeing if there are any findings")
         findings = FindingSets()
@@ -195,10 +210,15 @@ def invoke_semgrep(
                     "--json",
                     *rewrite_args,
                     *config_args,
+                    *user_provided_args,
                 ]
                 for path in chunk:
                     args.append(path)
-                semgrep_results = json.loads(str(semgrep(*args)))["results"]
+                semgrep_command = semgrep(*args)
+                try:
+                    semgrep_results = json.loads(str(semgrep_command))["results"]
+                except (JSONDecodeError, KeyError):
+                    raise SemgrepCommandFailure(semgrep_command)
                 findings.current.update_findings(
                     Finding.from_semgrep_result(result, committed_datetime)
                     for result in semgrep_results
@@ -243,10 +263,15 @@ def invoke_semgrep(
                         "--json",
                         *rewrite_args,
                         *config_args,
+                        *user_provided_args,
                     ]
                     for path in chunk:
                         args.append(path)
-                    semgrep_results = json.loads(str(semgrep(*args)))["results"]
+                    semgrep_command = semgrep(*args)
+                    try:
+                        semgrep_results = json.loads(str(semgrep_command))["results"]
+                    except (JSONDecodeError, KeyError):
+                        raise SemgrepCommandFailure(semgrep_command)
                     findings.baseline.update_findings(
                         Finding.from_semgrep_result(result, committed_datetime)
                         for result in semgrep_results
@@ -277,6 +302,8 @@ def scan(
     head_ref: Optional[str],
     semgrep_ignore: TextIO,
     uses_managed_policy: bool,
+    *,
+    semgrep_opts: str,
 ) -> Results:
     before = time.time()
     try:
@@ -287,8 +314,21 @@ def scan(
             head_ref,
             semgrep_ignore,
             uses_managed_policy,
+            semgrep_opts=semgrep_opts,
         )
-    except sh.ErrorReturnCode as error:
+    except (sh.ErrorReturnCode, SemgrepCommandFailure) as error:
+        if not semgrep_opts:
+            next_steps = (
+                "This is an internal error, please file an issue at "
+                "https://github.com/returntocorp/semgrep-action/issues/new/choose "
+                "and include any log output from above."
+            )
+        else:
+            next_steps = (
+                f"You've passed these custom semgrep options: '{semgrep_opts}'. "
+                "Custom options can easily break the internals of semgrep-agent, "
+                "so please reproduce this error without them before opening an issue."
+            )
         message = f"""
         === failed command's STDOUT:
 
@@ -300,8 +340,7 @@ def scan(
 
         === [ERROR] `{error.full_cmd}` failed with exit code {error.exit_code}
 
-        This is an internal error, please file an issue at https://github.com/returntocorp/semgrep-action/issues/new/choose
-        and include any log output from above.
+        {next_steps}
         """
         message = dedent(message).strip()
         click.echo("", err=True)

stubs/sh/__init__.pyi
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Union
+from typing import Any, Callable, Sequence, Union
 
 class ErrorReturnCode(Exception):
     @property
@@ -29,6 +29,7 @@ class Command(GitSubcommandsMixin):
     def bake(self, *args: Any, **kwargs: Any) -> Command: ...
 
 class RunningCommand(str, GitSubcommandsMixin):
+    cmd: Sequence[bytes]
     @property
     def stdout(self) -> bytes: ...
     @property

commit 909cb1fc8d562cde93c0a1863f69eb2a54693761

Add testing for semgrep-action with and without a stdin
Commit 550275f introduced a regression where semgrep-action would be
blocked listening to stdin. Our testing could not catch this regression
as tests always ran without a stdin connected the semgrep-action binary.

This commit allocates a pseudoterminal in testing in order to test this
behavior. For completeness, we also test runs without a pseudoterminal
by piping bash success (`:`).

tests/acceptance/disconnected-generic/base-log.out
@@ -1,0 +1,8 @@
+commit
+Author: Test <test@r2c.dev>
+Date:
+
+    base
+
+ foo.py | 1 +
+ 1 file changed, 1 insertion(+)

tests/acceptance/disconnected-generic/commands.yaml
@@ -1,0 +1,11 @@
+steps:
+  - include: generic.yaml
+  - name: disconnected agent
+    description: run agent on a codebase with no stdin
+    command:
+      - bash
+      - -c
+      - ": | semgrep-agent --config p/r2c-ci"
+    returncode: 1
+    expected_out: disconnected-agent.out
+    expected_err: disconnected-agent.err

tests/acceptance/disconnected-generic/disconnected-agent.err
@@ -1,0 +1,14 @@
+| versions          - semgrep 0.37.0 on Python
+Note that no baseline-ref was passed and detected environment is not Github or Gitlab to will default to perfoming a full scan. If you want semgrep to only report on new findings from a pull request please set baseline-ref
+| environment       - running in environment git, triggering event is 'unknown'
+| manage            - not logged in
+=== setting up agent configuration
+| using semgrep rules from https://semgrep.dev/c/p/r2c-ci
+| using default path ignore rules of common test and dependency directories
+| found 2 files in the paths to be scanned
+| skipping 1 file based on path ignore rules
+=== looking for current issues in 1 file
+| 1 current issue found
+| No ignored issues found
+=== not looking at pre-existing issues since all files with current issues are newly created
+=== exiting with failing status

tests/acceptance/disconnected-generic/disconnected-agent.out
@@ -1,0 +1,8 @@
+python.lang.correctness.useless-eqeq.useless-eqeq
+     > foo.py:1
+     ╷
+    1│   10 == 10
+     ╵
+     = This expression is always True: `10 == 10` or `10 != 10`. If testing for
+       floating point NaN, use `math.isnan(10)`, or `cmath.isnan(10)` if the
+       number is complex.

tests/acceptance/disconnected-generic/push-agent.err
@@ -1,0 +1,14 @@
+| versions          - semgrep 0.37.0 on Python
+Note that no baseline-ref was passed and detected environment is not Github or Gitlab to will default to perfoming a full scan. If you want semgrep to only report on new findings from a pull request please set baseline-ref
+| environment       - running in environment git, triggering event is 'unknown'
+| manage            - not logged in
+=== setting up agent configuration
+| using semgrep rules from https://semgrep.dev/c/p/r2c-ci
+| using default path ignore rules of common test and dependency directories
+| found 2 files in the paths to be scanned
+| skipping 1 file based on path ignore rules
+=== looking for current issues in 1 file
+| 1 current issue found
+| No ignored issues found
+=== not looking at pre-existing issues since all files with current issues are newly created
+=== exiting with failing status

tests/acceptance/disconnected-generic/push-agent.out
@@ -1,0 +1,8 @@
+python.lang.correctness.useless-eqeq.useless-eqeq
+     > foo.py:1
+     ╷
+    1│   10 == 10
+     ╵
+     = This expression is always True: `10 == 10` or `10 != 10`. If testing for
+       floating point NaN, use `math.isnan(10)`, or `cmath.isnan(10)` if the
+       number is complex.

tests/acceptance/qa.py
@@ -1,4 +1,5 @@
 import os
+import pty
 import re
 import subprocess
 import sys
@@ -87,10 +88,19 @@ def check_command(step: Any, pwd: str, target: str, rewrite: bool) -> None:
 
     print(f"======= {test_identifier} ========")
 
+    # In order to test behavior with a stdin, we create a pseudoterminal for the command here.
+    # To test behavior _without_ a stdin, set the "command" as:
+    # command:
+    #   - bash
+    #   - -c
+    #   - ": | <command>"
+    master_fd, slave_fd = pty.openpty()
+
     runned = subprocess.run(
         substituted,
         cwd=pwd,
         env=env,
+        stdin=slave_fd,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         encoding="utf-8",