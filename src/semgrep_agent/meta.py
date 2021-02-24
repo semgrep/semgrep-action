695277002d11b696d1317f5fb4c2ff6649eb3ff8
Bence Nagy <bence@underyx.me>
2021-02-24 18:19:40 +0100

    Make workflow work with pull_request_target event

--- src/semgrep_agent/meta.py
+++ src/semgrep_agent/meta.py
@@ -137,6 +137,13 @@ class GithubMeta(GitMeta):
         return {}
 
     @cachedproperty
+    def event_kind(self) -> str:
+        """Return common event name, ignoring variant types such as `pull_request_target`."""
+        if self.event_name == "pull_request_target":
+            return "pull_request"
+        return self.event_name or "unknown"
+
+    @cachedproperty
     def repo_name(self) -> Optional[str]:
         return os.getenv("GITHUB_REPOSITORY", "[unknown]")
 
@@ -148,7 +155,7 @@ class GithubMeta(GitMeta):
 
     @cachedproperty
     def commit_sha(self) -> Optional[str]:
-        if self.event_name == "pull_request":
+        if self.event_kind == "pull_request":
             # https://github.community/t/github-sha-not-the-same-as-the-triggering-commit/18286/2
             return self.glom_event(T["pull_request"]["head"]["sha"])  # type: ignore
         if self.event_name == "push":
@@ -157,7 +164,7 @@ class GithubMeta(GitMeta):
 
     @cachedproperty
     def head_ref(self) -> Optional[str]:
-        if self.event_name == "pull_request":
+        if self.event_kind == "pull_request":
             return self.commit_sha  # type: ignore
         else:
             return None
@@ -202,7 +209,7 @@ class GithubMeta(GitMeta):
     def base_commit_ref(self) -> Optional[str]:
         if self.cli_baseline_ref:
             return self.cli_baseline_ref
-        if self.event_name == "pull_request" and self.head_ref is not None:
+        if self.event_kind == "pull_request" and self.head_ref is not None:
             # The pull request "base" that GitHub sends us is not necessarily the merge base,
             # so we need to get the merge-base from Git
             return self._find_branchoff_point()
@@ -238,7 +245,7 @@ class GithubMeta(GitMeta):
         return str(pr_title) if pr_title else None
 
     def initialize_repo(self) -> None:
-        if self.event_name == "pull_request" and self.head_ref is not None:
+        if self.event_kind == "pull_request" and self.head_ref is not None:
             self._find_branchoff_point()
         return
 
