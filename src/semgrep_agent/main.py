23b5fa7e2f5686b9cc1319ba3626e3de9295c8fb
Sabrina Brogren <sabrina@returntocorp.com>
2021-05-25 15:25:37 -0400

    Update src/semgrep_agent/main.py
    Co-authored-by: Clara McCreery <clara@returntocorp.com>

--- src/semgrep_agent/main.py
+++ src/semgrep_agent/main.py
@@ -88,7 +88,7 @@ def url(string: str) -> str:
     "--publish-deployment",
     envvar="INPUT_PUBLISHDEPLOYMENT",
     type=int,
-    help="Your semgrep.dev organization ID (requires --publish-token)",
+    help="Your semgrep.dev deployment ID (requires --publish-token)",
 )
 @click.option(
     "--publish-url",
