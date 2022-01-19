839eccd628b2c33d91f75deacf8fc69ab0ec2ee5
Matthew McQuaid <matthew@returntocorp.com>
2022-01-19 16:14:11 -0500

    add file to be ignored by app

---
+++ test_app_ignore.py
@@ -0,0 +1,3 @@
+# This should be ignored by semgrep-app. If this file is ever caught when running semgrep-action
+# it is an indication that semgrep-action is not repsecting ignore rules from semgrep-app
+x = 5 == 5
