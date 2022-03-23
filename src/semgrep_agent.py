bb59ba9a023a0f50dd2d607a59b130f42a972284
Bence Nagy <nagy@bence.dev>
2022-03-23 16:05:45 -0700

    Print deprecation notice when audit mode is used

--- src/semgrep_agent.py
+++ src/semgrep_agent.py
@@ -5,23 +5,13 @@ import sys
 import textwrap
 
 
-def run_sarif_scan() -> None:
-    cmd = ["semgrep", "scan", "--sarif", "--output=semgrep.sarif"]
-
-    if os.environ.get("SEMGREP_APP_TOKEN"):
-        cmd.append("--config=policy")
-
+def print_deprecation_notice(message: str) -> None:
     print(
         textwrap.dedent(
             f"""
                 =========== DEPRECATION WARNING ===========
-                Semgrep's SARIF file generation is changing by 2022 May.
-                Please update your CI configuration. Your CI script should run this command to get a SARIF file:
-                  $ {' '.join(cmd)}
 
-                The above command will always succeed, even if there are findings.
-                To report pull request failures and optionally push findings to semgrep.dev, add a separate job that runs:
-                  $ semgrep ci
+                {textwrap.dedent(message).strip()}
 
                 For questions or support, please reach out at https://r2c.dev/slack
             """
@@ -29,12 +19,44 @@ def run_sarif_scan() -> None:
         + "\n\n",
         file=sys.stderr,
     )
+
+
+def run_sarif_scan() -> None:
+    cmd = ["semgrep", "scan", "--sarif", "--output=semgrep.sarif"]
+
+    if os.environ.get("SEMGREP_APP_TOKEN"):
+        cmd.append("--config=policy")
+
+    print_deprecation_notice(
+        """
+        Semgrep's SARIF file generation is changing by 2022 May.
+        Please update your CI configuration. Your CI script should run this command to get a SARIF file:
+            $ {' '.join(cmd)}
+
+        The above command will always succeed, even if there are findings.
+        To report pull request failures and optionally push findings to semgrep.dev, add a separate job that runs:
+            $ semgrep ci
+        """
+    )
+
     subprocess.run(cmd)
 
 
 def main() -> None:
     if os.environ.get("INPUT_GENERATESARIF", "0") == "1":
         run_sarif_scan()
+
+    if os.environ.get("INPUT_AUDITON", ""):
+        print_deprecation_notice(
+            """
+            Semgrep's audit mode setting will be removed by 2022 May.
+
+            Please update your CI configuration. Your CI script should run this command to ignore findings:
+                $ semgrep ci || true
+
+            We recommend setting up separate CI jobs for pull request and push events, so that you can ignore findings only on push events.
+            """
+        )
 
     os.execvp("semgrep", ["semgrep", "ci"])
 
